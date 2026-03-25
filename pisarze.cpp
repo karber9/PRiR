57b6e08154b6a11d9cea27f96a1dbf156185b116
karber9 <150918920+karber9@users.noreply.github.com>
2026-03-25 11:34:54 +0100

    Update pisarze.cpp

--- pisarze.cpp
+++ pisarze.cpp
@@ -7,28 +7,29 @@
 #include <random>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 struct Buffer {
-    int  val     = 0;
-    int  reads   = 0;
-    bool busy    = false;
-    int  version = 0;
+    int val = 0;
+    int reads = 0;
+    bool busy = false;
+    int version = 0;
 };
 
 struct StanCzytelnika {
-    string status    = "czeka";
-    int    czyta_val = -1;
+    string status = "czeka";
+    int czyta_val = -1;
 };
 
 class Kelner {
-    mutex              m;
+    mutex m;
     condition_variable cv_p, cv_c;
-    queue<int>         fifo;
-    int                ilosc_czytelnikow = 0;
-    bool               ktos_pisze        = false;  
-    vector<Buffer>     bufory;
-    int                limit_odczytow    = 3;
+    queue<int> fifo;
+    int ilosc_czytelnikow = 0;
+    bool ktos_pisze = false;  
+    vector<Buffer> bufory;
+    int limit_odczytow= 3;
 
 public:
     vector<StanCzytelnika> stan_cz;
@@ -46,17 +47,17 @@ public:
         });
 
         fifo.pop();
-        bufory[id].busy  = true;
+        bufory[id].busy= true;
         bufory[id].reads = 0;
-        ktos_pisze       = true;         
+        ktos_pisze = true;         
     }
 
     void pisarz_konczy(int id, int v) {
         lock_guard<mutex> lock(m);
-        bufory[id].val     = v;
-        bufory[id].busy    = false;
+        bufory[id].val= v;
+        bufory[id].busy= false;
         bufory[id].version++;
-        ktos_pisze         = false;      
+        ktos_pisze= false;      
         cv_c.notify_all();
         cv_p.notify_all();
     }
@@ -74,14 +75,16 @@ public:
         cv_c.wait(lock, [&]{
             for (int i = 0; i < (int)bufory.size(); i++) {
                 if (!bufory[i].busy && bufory[i].version > znane_wersje[i]) {
-                    b_id = i; return true;
+                    b_id = i;
+                    return true;
                 }
             }
             return false;
         });
         ilosc_czytelnikow++;
-        stan_cz[cz_id].status    = "czyta P" + to_string(b_id);
+        stan_cz[cz_id].status= "czyta P" + to_string(b_id);
         stan_cz[cz_id].czyta_val = bufory[b_id].val;
+        
         return {bufory[b_id].val, bufory[b_id].version, b_id};
     }
 
@@ -89,7 +92,7 @@ public:
         lock_guard<mutex> lock(m);
         if (b_id != -1) bufory[b_id].reads++;
         ilosc_czytelnikow--;
-        stan_cz[cz_id].status    = "odpoczywa";
+        stan_cz[cz_id].status= "odpoczywa";
         stan_cz[cz_id].czyta_val = -1;
         cv_p.notify_all();
     }
@@ -103,24 +106,27 @@ public:
         for (int i = 0; i < n_p; i++) {
             string st;
             if (bufory[i].busy) st = "ZAPISUJE";
+                
             else if (bufory[i].reads < limit_odczytow && bufory[i].version > 0)
                 st = "OCZEKIWANY (" + to_string(bufory[i].reads) + "/3)";
+                
             else st = "KOLEJKA";
-            mvprintw(2+i, 0, "P%-5d | %7d | %7d | %s",
-                     i, bufory[i].val, bufory[i].reads, st.c_str());
+            mvprintw(2+i, 0, "P%-5d | %7d | %7d | %s", i, bufory[i].val, bufory[i].reads, st.c_str());
         }
         int row = 3 + n_p;
         mvprintw(row++, 0, "KOLEJKA FIFO: ");
         queue<int> tmp = fifo;
-        while (!tmp.empty()) { printw("%d ", tmp.front()); tmp.pop(); }
+        while (!tmp.empty()) { 
+            printw("%d ", tmp.front()); tmp.pop(); 
+        }
                 
         row++;
         mvprintw(row++, 0, "CZYTELNIK  STATUS       CZYTA");
         
         for (int i = 0; i < n_c; i++) {
             auto& s = stan_cz[i];
-            mvprintw(row++, 0, "C%-8d | %-11s | %s", i, s.status.c_str(),
-                     s.czyta_val >= 0 ? to_string(s.czyta_val).c_str() : "---");
+            mvprintw(row++, 0, "C%-8d | %-11s | %s", i, s.status.c_str(), 
+                s.czyta_val >= 0 ? to_string(s.czyta_val).c_str() : "---");
         }
         
         refresh();
@@ -156,7 +162,7 @@ void f_czytelnik(int id, Kelner& k, int n_p) {
 
 int main() {
     int n_p, n_c;
-    cout << "Liczba pisarzy: ";    cin >> n_p;
+    cout << "Liczba pisarzy: ";cin >> n_p;
     cout << "Liczba czytelnikow: "; cin >> n_c;
     if (n_p <= 0 || n_c <= 0) return 0;
 
@@ -164,7 +170,7 @@ int main() {
 
     Kelner k(n_p, n_c);
     vector<thread> ths;
-    for (int i = 0; i < n_p; i++) ths.emplace_back(f_pisarz,    i, ref(k));
+    for (int i = 0; i < n_p; i++) ths.emplace_back(f_pisarz, i, ref(k));
     for (int i = 0; i < n_c; i++) ths.emplace_back(f_czytelnik, i, ref(k), n_p);
 
     while (true) {
